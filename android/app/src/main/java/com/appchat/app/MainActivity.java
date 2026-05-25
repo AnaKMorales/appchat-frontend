beef63d2bebd61920ff8a7028fa45ae3c5a18ef4
AnaMOrales <ana.morales@estudiantes.utec.edu.uy>
2026-05-24 21:15:08 -0300

    mobile

---
+++ android/app/src/main/java/com/appchat/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.appchat.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.appchat.app',
+  appName: 'AppChat',
+  webDir: 'build'
+};
+
+export default config;
